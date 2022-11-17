86bf97351eafa7fcfd5c68223733e7633ef9d901
CHW <gpdnjs38953@naver.com>
2022-11-17 19:15:09 +0900

    sql 수정

--- 로컬-FOODDB.sql
+++ 로컬-FOODDB.sql
@@ -1,35 +1,49 @@
+drop table tbl_product;
 
 create sequence seq_food;
 create table tbl_product(
-    pid varchar2(50) not null,
+    pid number(10,0) not null,
     pname varchar2(50) not null,
     pkind varchar2(50) not null,
     pcontent varchar2(300),
     pprice number not null,
     pimg varchar2(50)
 );
-alter table tbl_product add CONSTRAINT pk_board primary key (pid);
-commit;
+alter table tbl_product add CONSTRAINT pk_product primary key (pid);
 
+create sequence seq_cart;
 create table tbl_cart(
-    pid varchar2(50) not null,
+    cno number(10,0) not null,
+    pid number(10,0) not null,
     pname varchar2(50) not null,
     pprice number not null,
+    pcontent varchar2(300),
     pimg varchar2(50),
     pcount number not null
 );
+alter table tbl_cart add CONSTRAINT pk_cart primary key (cno);
+alter table tbl_cart add CONSTRAINT fk_cart FOREIGN key (pid) references tbl_product(pid);
 
+create sequence seq_wishlist;
 create table tbl_wishlist(
-    pid varchar2(50) not null,
+    wno number(10,0) not null,
+    pid number(10,0) not null,
     pname varchar2(50) not null,
     pprice number not null,
+    pcontent varchar2(300),
     pimg varchar2(50),
     pcount number not null
 );
+alter table tbl_wishlist add CONSTRAINT pk_wishlist primary key (wno);
+alter table tbl_wishlist add CONSTRAINT fk_wishlist FOREIGN key (pid) references tbl_product(pid);
 
+create sequence seq_checkout;
 create table tbl_checkout(
-    id varchar2(50) not null,
+    id number(10,0) not null,
     name varchar2(50) not null,
     adrress varchar2(100),
     psum number not null
 );
+alter table tbl_checkout add CONSTRAINT pk_checkout primary key (id);
+
+commit;
