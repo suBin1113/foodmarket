fff383ff07446cc82644dd6d82617145b754aac5
Hong <hong@gmail.com>
2022-11-21 18:50:39 +0900

    자료

---
+++ src/main/java/com/spring/domain/WishlistVO.java
@@ -0,0 +1,14 @@
+package com.spring.domain;
+
+import lombok.Data;
+
+@Data
+public class WishlistVO {
+	private int wno;
+	private int pid;
+	private String pname;
+	private int pprice;
+	private String pcontent;
+	private String pimg;
+	private int pcount;
+}
