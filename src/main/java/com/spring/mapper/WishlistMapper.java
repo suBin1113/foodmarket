d1abaeaee699a72d8a02068438d9dcdde57321fe
Hong <hong@gmail.com>
2022-11-26 10:07:30 +0900

    수정

--- src/main/java/com/spring/mapper/WishlistMapper.java
+++ src/main/java/com/spring/mapper/WishlistMapper.java
@@ -10,8 +10,8 @@ public interface WishlistMapper {
 	public List<WishlistVO> getListWithPaging(Criteria cri);
 	public void insert(WishlistVO board);
 	public void insertSelectKey(WishlistVO board);
-	public WishlistVO read(Long bno);
-	public int delete(Long bno);
+	public WishlistVO read(Long wno);
+	public int delete(Long wno);
 	public int update(WishlistVO board);
 	public int getTotalCount(Criteria cri);
 }
