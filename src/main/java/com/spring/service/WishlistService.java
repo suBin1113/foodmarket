068af56f747363ca92cda083cbe4238367cfed11
Hong <hong@gmail.com>
2022-11-26 13:42:48 +0900

    잘못된 부분 수정

--- src/main/java/com/spring/service/WishlistService.java
+++ src/main/java/com/spring/service/WishlistService.java
@@ -6,11 +6,8 @@ import com.spring.domain.Criteria;
 import com.spring.domain.WishlistVO;
 
 public interface WishlistService {
-	public void register(WishlistVO vo);
-	public WishlistVO get(Long wno);
 	public int modify(WishlistVO vo);
 	public List<WishlistVO> getList();
-	public List<WishlistVO> getList(Criteria cri);
 	public int getTotal(Criteria cri);
 	public int deleteWishlist(Long wno);
 }
