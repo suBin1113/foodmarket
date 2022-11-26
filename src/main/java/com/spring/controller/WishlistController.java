3829655da1634fe3ae2dfb6394478b7c69610d08
Hong <hong@gmail.com>
2022-11-26 10:06:51 +0900

    delete 부분

--- src/main/java/com/spring/controller/WishlistController.java
+++ src/main/java/com/spring/controller/WishlistController.java
@@ -5,27 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import com.spring.domain.WishlistDTO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.spring.service.WishlistService;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
+@RequestMapping("/foodMarket/*")
 public class WishlistController {
 	@Autowired
 	private WishlistService service;
-	
-	@GetMapping("foodMarket/wishlist")
+
+	@GetMapping("/wishlist")
 	public void uploadForm(Model model) {
 		model.addAttribute("wishlist", service.getList());
 	}
-	
+
 	@PostMapping("/wishlist/delete")
-	public String deleteWishlist(WishlistDTO wish) {
-		service.deleteWishlist(wish.getWno());
-		
-		return "redirect:/wishlist/" + wish.getWno();
+	public String deleteWishlist(Long wno, RedirectAttributes rttr) {
+		log.info("delete..." + wno);
+		if(service.deleteWishlist(wno) == 1) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/foodMarket/wishlist/";
 	}
 }
