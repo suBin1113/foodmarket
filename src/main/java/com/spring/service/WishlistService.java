93edf0485d829b4ea09b03e3213d76b70a7ca339
coofang <118031271+coofang@users.noreply.github.com>
2022-11-28 21:36:22 +0900

    내용 추가 및 수정

--- src/main/java/com/spring/service/WishlistService.java
+++ src/main/java/com/spring/service/WishlistService.java
@@ -2,12 +2,12 @@ package com.spring.service;
 
 import java.util.List;
 
-import com.spring.domain.Criteria;
+import com.spring.domain.WishlistDTO;
 import com.spring.domain.WishlistVO;
 
 public interface WishlistService {
-	public int modify(WishlistVO vo);
 	public List<WishlistVO> getList();
-	public int getTotal(Criteria cri);
-	public int deleteWishlist(Long wno);
+	public int wishDelete(Long wno);
+	void insert(WishlistDTO dto);
+	
 }
--- src/main/java/com/spring/service/WishlistServiceImpl.java
+++ src/main/java/com/spring/service/WishlistServiceImpl.java
@@ -5,66 +5,33 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.spring.domain.WishlistDTO;
 import com.spring.domain.WishlistVO;
-import com.spring.domain.Criteria;
 import com.spring.mapper.WishlistMapper;
 
-import jdk.internal.org.jline.utils.Log;
+import lombok.extern.log4j.Log4j;
 
 @Service
+@Log4j
 public class WishlistServiceImpl implements WishlistService {
 	@Autowired
 	private WishlistMapper mapper;
 
 	@Override
-	public void register(WishlistVO vo) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public WishlistVO get(Long wno) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int modify(WishlistVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
 	public List<WishlistVO> getList() {
-		// TODO Auto-generated method stub
-		return null;
+		return mapper.getList();
 	}
 
 	@Override
-	public List<WishlistVO> getList(Criteria cri) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getTotal(Criteria cri) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public WishlistMapper getMapper() {
-		return mapper;
-	}
-
-	public void setMapper(WishlistMapper mapper) {
-		this.mapper = mapper;
-	}
-
-
-	@Override
-	public int deleteWishlist(Long wno) {
-		Log.info("remove..." + wno);
+	public int wishDelete(Long wno) {
+		log.info("remove..." + wno);
 		return mapper.delete(wno);
+	}
+
+	@Override
+	public void insert(WishlistDTO dto) {
+		
+		
 	}
 
 }
